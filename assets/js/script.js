630a66190496d26e89a24ffa176d5dabff785a04
rvanooyen <robert.vanooyen@gmail.com>
2021-08-18 17:28:51 -0500

    check if passwordLength is a number and between 8 and 128

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,27 @@ var writePassword = function() {
 
 // function to generate a new password
 var generatePassword = function() {
+  
   // local variable declarations
   var lowerCaseString;
   var upperCaseString;
   var numberString;
   var specialCharString;
-  var passwordLength = prompt("How long would like the password to be? Between 8 and 128 characters.");  
+  var passwordLength = 0;  
   var lowerCaseConfirm = confirm("Would you like to include lower case letters in the password?");
   var upperCaseConfirm = confirm("Would you like to include upper case letters in the password?");
   var numberConfirm = confirm("Would you like to include numbers in the password?");
   var specialCharConfirm = confirm("Would you like to include special characters in the password?");
 
+  // checks if passwordLength is between 8 and 128
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("How long would like the password to be? Between 8 and 128 characters.");
+    while (isNaN(passwordLength)) {
+      passwordLength = prompt("You have entered an invalid number. Please enter a number from 8 to 128");
+    }
+    passwordLength = Number(passwordLength);
+  }
+  
   console.log(passwordLength);
   console.log(lowerCaseConfirm);
   console.log(upperCaseConfirm);
